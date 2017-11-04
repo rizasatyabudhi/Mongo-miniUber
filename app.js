e697ae623592668eabc81c2081f2b33a7fb9231d
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-11-04 21:19:02 +0700

    new instance & test

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const routes = require("./routes/routes");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/miniUber");
+
+// bodyParser middleware must be placed before routes(app)
+app.use(bodyParser.json());
 routes(app);
+
 module.exports = app;
--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -1,5 +1,12 @@
+const Driver = require("../models/driver");
+
 module.exports = {
   greetings(req, res) {
     res.send({ hi: "there" });
+  },
+
+  create(req, res) {
+    const driverProps = req.body;
+    Driver.create(driverProps).then(driver => res.send(driver));
   }
 };
